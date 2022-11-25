e9f98cef35e7fb9f48a6abdfd50d5e62350e12ac
Neel <neelnarayanshetty@gmail.com>
2022-11-25 15:53:40 +0530

    inital commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Navigator from './src/navigator/Navigator';
+
+const App = () => {
+  return (
+    <>
+      <Navigator />
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
